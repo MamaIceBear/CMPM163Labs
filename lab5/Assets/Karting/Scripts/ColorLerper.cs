2130216d44df839bf01da6a5c2463d79b55c1aef
MamaIceBear <snguye39@ucsc.edu>
2020-05-05 02:37:25 -0700

    Lab5

---
+++ lab5/Assets/Karting/Scripts/ColorLerper.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorLerper : MonoBehaviour
+{
+    public float speed;
+    public Color startColor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
